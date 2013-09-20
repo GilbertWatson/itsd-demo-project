832c02793ef6631bd480bafb2080de2a6ac9af08
Gilbert Watson <gwatson@ubuntu12-04.64bitVM1>
2013-09-20 09:47:16 -0400

    changed size of fake data

--- mutliprocessingdemo.r
+++ mutliprocessingdemo.r
@@ -1,10 +1,11 @@
 require(snowfall)
 
 #make some fake data
-x <- data.frame(one = sample(x = seq(1, 100, 1),size = 2000000, replace = T),
-                two = sample(x = seq(1, 100, 1),size = 2000000, replace = T),
-                three = sample(x = seq(1, 100, 1),size = 2000000, replace = T),
-                four = sample(x = seq(1, 100, 1),size = 2000000, replace = T))
+sizex = 200000
+x <- data.frame(one = sample(x = seq(1, 100, 1),size = sizex, replace = T),
+                two = sample(x = seq(1, 100, 1),size = sizex, replace = T),
+                three = sample(x = seq(1, 100, 1),size = sizex, replace = T),
+                four = sample(x = seq(1, 100, 1),size = sizex, replace = T))
 
 #one processor
  one_p <- system.time(apply(X = x, MARGIN = 1, FUN = function(x) {return(mean(x, na.rm = T))}))
